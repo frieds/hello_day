b2015d27665d34ffd438894ab68231075f786dd4
Dan Friedman <dan.friedman@electric.ai>
2023-02-12 18:36:50 -0500

    get hourly weather granular data; model responses;

--- external_apis/weather/client.py
+++ external_apis/weather/client.py
@@ -2,14 +2,30 @@ from requests import get
 from requests.exceptions import HTTPError
 from enum import Enum
 
-from external_apis.weather.schemas import HourlyWeatherResponse, HourlyWeatherPeriodDetails
+from external_apis.weather.schemas import HourlyWeatherResponse, HourlyWeatherPeriodDetails, \
+    HourlyWeatherGranularResponse
 
 _BASE_URL = "https://api.weather.gov"
 
 
 class EndpointPath(str, Enum):
     HOURLY_FORECAST = "/gridpoints/OKX/{grid_x},{grid_y}/forecast/hourly"
+    HOURLY_GRANULAR = "/gridpoints/LWX/{grid_x},{grid_y}"
 
+
+def _get_hourly_granular(grid_x: int, grid_y: int):
+    endpoint = EndpointPath.HOURLY_GRANULAR.format(grid_x=grid_x, grid_y=grid_y)
+    url = f"{_BASE_URL}{endpoint}"
+
+    try:
+        response = get(url=url, headers={"User-Agent": "(local app, dfriedman33@gmail.com)"})
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+        raise http_err
+    return HourlyWeatherGranularResponse.parse_obj(response.json())
+    
     
 def _get_hourly_forecast(grid_x: int, grid_y: int) -> HourlyWeatherResponse:
     endpoint = EndpointPath.HOURLY_FORECAST.format(grid_x=grid_x, grid_y=grid_y)
@@ -30,11 +46,3 @@ def get_hourly_weather_details_now(grid_x: int, grid_y: int) -> HourlyWeatherPer
     """Assumption is first period is relevant weather now"""
     hourly_weather_response = _get_hourly_forecast(grid_x, grid_y)
     return hourly_weather_response.properties.periods[0]
-
-
-
-
-
-
-
-
--- external_apis/weather/schemas.py
+++ external_apis/weather/schemas.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel, Field
-from typing import Any
+from pydantic import BaseModel, Field, Extra
+from typing import Any, List
+from datetime import datetime
+from dateutil.parser import parse
 
 
 class HourlyWeatherPeriodDetails(BaseModel):
@@ -39,3 +41,48 @@ class HourlyWeatherResponse(BaseModel):
     type: str
     geometry: dict
     properties: HourlyWeatherProperties
+
+
+class HourlyWeatherGranularPropertyPeriodValues(BaseModel):
+    validTime: str
+    value: float
+    
+    @property
+    def start_time(self) -> datetime:
+        return parse(self.validTime.split('/')[0])
+    
+
+class HourlyWeatherGranularPropertyDetails(BaseModel):
+    unit_of_measurement: str = Field(alias="uom")
+    values: List[HourlyWeatherGranularPropertyPeriodValues]
+
+    @property
+    def unit_of_measurement_value(self) -> str:
+        return self.unit_of_measurement.split(":")[1]
+
+    @property
+    def unit_of_measurement_value_full_name(self):
+        # only mapped measurements for metrics used
+        mapping = {"degC": "degrees_celsius",
+                   "percent": "percent",
+                   "mm": "millimeters",
+                   "km_h-1": "kilometre_per_hour"
+                   }
+        return mapping[self.unit_of_measurement_value]
+    
+
+class HourlyWeatherGranularProperties(BaseModel, extra=Extra.allow):
+    # arg of extra set to allow ignores tens of other fields
+    sky_cover: HourlyWeatherGranularPropertyDetails = Field(alias="skyCover")
+    apparent_temperature: HourlyWeatherGranularPropertyDetails = Field(alias="apparentTemperature")
+    probability_of_precipitation: HourlyWeatherGranularPropertyDetails = Field(alias="probabilityOfPrecipitation")
+    quantitative_precipitation: HourlyWeatherGranularPropertyDetails = Field(alias="quantitativePrecipitation")
+    wind_speed: HourlyWeatherGranularPropertyDetails = Field(alias="windSpeed")
+        
+
+class HourlyWeatherGranularResponse(BaseModel):
+    context: list = Field(alias="@context")
+    type: str
+    geometry: dict
+    id: str
+    properties: HourlyWeatherGranularProperties
