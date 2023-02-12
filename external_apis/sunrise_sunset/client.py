31f13809e8fa64c1d43896336b2c8fc9f444b033
Dan Friedman <dfriedman33@gmail.com>
2023-02-11 20:15:15 -0500

    feat: get sunrise sunset data; output next sunrise time (#4)

---
+++ external_apis/sunrise_sunset/client.py
@@ -0,0 +1,27 @@
+from requests import get
+from enum import Enum
+from requests.exceptions import HTTPError
+from datetime import date
+
+from external_apis.sunrise_sunset.schemas import SunriseSunsetResponse
+
+_BASE_URL = "https://api.sunrise-sunset.org"
+
+
+class EndpointPath(str, Enum):
+    # formatted=0 ensures datetimes in ISO 8601 format and day_length in seconds
+    SUNRISE_SUNSET_TIMES = "/json?lat={latitude}&lng={longitude}&date={date}&formatted=0"
+
+
+def get_sunrise_sunset_times(latitude: float, longitude: float, date: date) -> SunriseSunsetResponse:
+    endpoint = EndpointPath.SUNRISE_SUNSET_TIMES.format(latitude=latitude, longitude=longitude, date=date)
+    url = f"{_BASE_URL}{endpoint}"
+
+    try:
+        response = get(url=url)
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+        raise http_err
+
+    return SunriseSunsetResponse.parse_obj(response.json())
