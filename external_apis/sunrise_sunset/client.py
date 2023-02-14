58da9ea35f73591422c0478a33bdf6f4e451a04d
Dan Friedman <dan.friedman@electric.ai>
2023-02-14 09:39:02 -0500

    refactor get sunrise sunset logic so don't need to include date b/c default is today by api; refactor variable names

--- external_apis/sunrise_sunset/client.py
+++ external_apis/sunrise_sunset/client.py
@@ -1,3 +1,5 @@
+import datetime
+from typing import Optional
 from requests import get
 from requests.exceptions import HTTPError
 from datetime import date
@@ -7,9 +9,8 @@ from external_apis.sunrise_sunset.schemas import SunriseSunsetResponse, SunriseS
 _BASE_URL = "https://api.sunrise-sunset.org/json"
 
 
-def get_sunrise_sunset_times(latitude: float, longitude: float, date_value: date) -> SunriseSunsetResponse:
-    query_params = SunriseSunsetQueryParams(latitude=latitude, longitude=longitude, date=date_value)
-    
+def get_sunrise_sunset_times(latitude: float, longitude: float, date_value: Optional[date] = None) -> SunriseSunsetResponse:
+    query_params = SunriseSunsetQueryParams(latitude=latitude, longitude=longitude, date_value=date_value)
     try:
         response = get(url=_BASE_URL, params=query_params.to_query_param_names())
         response.raise_for_status()
