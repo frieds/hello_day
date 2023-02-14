17e6923ef016ac5208a1901699056a393093a511
Dan Friedman <dan.friedman@electric.ai>
2023-02-13 19:27:07 -0500

    refactor logic to recommend wearing sunglasses

--- external_apis/sunrise_sunset/client.py
+++ external_apis/sunrise_sunset/client.py
@@ -7,8 +7,8 @@ from external_apis.sunrise_sunset.schemas import SunriseSunsetResponse, SunriseS
 _BASE_URL = "https://api.sunrise-sunset.org/json"
 
 
-def get_sunrise_sunset_times(latitude: float, longitude: float, a_date: date) -> SunriseSunsetResponse:
-    query_params = SunriseSunsetQueryParams(latitude=latitude, longitude=longitude, date=a_date)
+def get_sunrise_sunset_times(latitude: float, longitude: float, date_value: date) -> SunriseSunsetResponse:
+    query_params = SunriseSunsetQueryParams(latitude=latitude, longitude=longitude, date=date_value)
     
     try:
         response = get(url=_BASE_URL, params=query_params.to_query_param_names())
