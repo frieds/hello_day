311a235321bbe4548f723605799ba2f602dea086
Dan Friedman <dan.friedman@electric.ai>
2023-02-14 13:20:08 -0500

    refactor sunrise api endpoint query params; refactor output for today's relevant sunrise time

--- external_apis/sunrise_sunset/client.py
+++ external_apis/sunrise_sunset/client.py
@@ -1,4 +1,3 @@
-import datetime
 from typing import Optional
 from requests import get
 from requests.exceptions import HTTPError
@@ -11,8 +10,9 @@ _BASE_URL = "https://api.sunrise-sunset.org/json"
 
 def get_sunrise_sunset_times(latitude: float, longitude: float, date_value: Optional[date] = None) -> SunriseSunsetResponse:
     query_params = SunriseSunsetQueryParams(latitude=latitude, longitude=longitude, date_value=date_value)
+    query_params_dict = query_params.dict(exclude_none=True, by_alias=True)
     try:
-        response = get(url=_BASE_URL, params=query_params.to_query_param_names())
+        response = get(url=_BASE_URL, params=query_params_dict)
         response.raise_for_status()
     except HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
--- main.py
+++ main.py
@@ -36,32 +36,21 @@ def _determine_level(weather_metric: Enum, value: int):
             return member
 
 
-def _determine_relevant_sunrise_date() -> date:
-    """Returns today's date if time now is between 4-7:30am; else, tomorrow date"""
-    local_tz = tz.tzlocal()
-    local_time_now = datetime.now(local_tz)
-    if 4 <= local_time_now.hour < 7 or (local_time_now.hour == 7 and local_time_now.minute < 30):
-        relevant_date = local_time_now.date()
+def _time_human_readable_string(datetime_value_utc: datetime):
+    # convert to local time
+    datetime_value_local = datetime_value_utc.astimezone(tz.tzlocal())
+    # converts to hour:minute am/pm
+    datetime_hour_minute = datetime_value_local.strftime("%-H:%M%p").lower()
+
+    datetime_now_utc = datetime.now(tz.tzutc())
+
+    if datetime_value_utc > datetime_now_utc:
+        output = f"is today at {datetime_hour_minute}"
+    elif datetime_value_utc > datetime_now_utc + timedelta(minutes=30):
+        output = f"was earlier at {datetime_hour_minute}"
     else:
-        relevant_date = local_time_now.date() + timedelta(days=1)
-    return relevant_date
-
-
-def _date_human_readable_string(a_datetime: datetime):
-    """Convert datetime into format like 6:51am today or 6:30am tomorrow"""
-    local_tz = tz.tzlocal()
-
-    # Get today's and tomorrow's date in the local time zone
-    today = datetime.now(local_tz).date()
-    tomorrow = today + timedelta(days=1)
-
-    datetime_hour_minute = a_datetime.strftime("%-H:%M%p").lower()
-    if a_datetime.date() == today:
-        date_str = "today"
-    elif a_datetime.date() == tomorrow:
-        date_str = "tomorrow"
-
-    return f"{datetime_hour_minute} {date_str}"
+        output = f"was way earlier at {datetime_hour_minute}"
+    return output
 
 
 def _round_datetime_to_hour(a_datetime):
@@ -72,9 +61,9 @@ def _round_datetime_to_hour(a_datetime):
     return a_datetime
 
 
-def _determine_now_time_period_value(metric_period_values, rounded_utc_hour_now) -> float:
+def _determine_now_time_period_value(metric_period_values, utc_now_rounded_hour) -> float:
     for period in reversed(metric_period_values):
-        if period.start_time <= rounded_utc_hour_now:
+        if period.start_time <= utc_now_rounded_hour:
             return period.value
 
 
@@ -86,45 +75,37 @@ def main():
     longitude = -73.929910
 
     utc_now = datetime.now(tz.tzutc())
-    rounded_utc_hour_now = _round_datetime_to_hour(utc_now)
+    utc_now_rounded_hour = _round_datetime_to_hour(utc_now)
 
     # Get weather metrics
     weather_granular_details = get_hourly_granular(grid_x, grid_y)
 
     apparent_temp_period_values = weather_granular_details.properties.apparent_temperature.values
-    apparent_temp_value_now = _determine_now_time_period_value(apparent_temp_period_values, rounded_utc_hour_now)
+    apparent_temp_value_now = _determine_now_time_period_value(apparent_temp_period_values, utc_now_rounded_hour)
 
     wind_speed_period_values = weather_granular_details.properties.wind_speed.values
-    wind_speed_value_now = _determine_now_time_period_value(wind_speed_period_values, rounded_utc_hour_now)
+    wind_speed_value_now = _determine_now_time_period_value(wind_speed_period_values, utc_now_rounded_hour)
 
     probability_of_precipitation_period_values = weather_granular_details.properties.probability_of_precipitation.values
     probability_of_precipitation_value_now = _determine_now_time_period_value(probability_of_precipitation_period_values
-                                                                              , rounded_utc_hour_now)
+                                                                              , utc_now_rounded_hour)
 
     quantitative_precipitation_period_values = weather_granular_details.properties.quantitative_precipitation.values
     quantitative_precipitation_value_now = _determine_now_time_period_value(quantitative_precipitation_period_values,
-                                                                            rounded_utc_hour_now)
+                                                                            utc_now_rounded_hour)
 
     sky_cover_period_values = weather_granular_details.properties.sky_cover.values
-    sky_cover_value_now = _determine_now_time_period_value(sky_cover_period_values, rounded_utc_hour_now)
+    sky_cover_value_now = _determine_now_time_period_value(sky_cover_period_values, utc_now_rounded_hour)
 
     # Get sunrise data
-    relevant_sunrise_date = _determine_relevant_sunrise_date()
-    sunrise_sunset_response = get_sunrise_sunset_times(latitude=latitude, longitude=longitude,
-                                                       date_value=relevant_sunrise_date)
-    local_tz = tz.tzlocal()
-    sunrise_time_utc = sunrise_sunset_response.results.sunrise_time_utc
-    sunrise_time_est = sunrise_time_utc.astimezone(local_tz)
-    sunrise_statement = _date_human_readable_string(sunrise_time_est)
+    sunrise_sunset_today_response = get_sunrise_sunset_times(latitude=latitude, longitude=longitude)
 
-    date_today = utc_now.date()
-    sunrise_sunset_today_response = get_sunrise_sunset_times(latitude=latitude, longitude=longitude, date_value=date_today)
-    sunrise_today_time_utc = sunrise_sunset_today_response.results.sunrise_time_utc
-    sunset_today_time_utc = sunrise_sunset_today_response.results.sunset_time_utc
+    sunrise_time_utc = sunrise_sunset_today_response.results.sunrise_time_utc
+    sunrise_time_human_readable_statement = _time_human_readable_string(sunrise_time_utc)
 
     # Logic to recommend clothing and accessories
-    is_past_morning = utc_now > sunrise_today_time_utc + timedelta(minutes=30)
-    is_before_evening = utc_now + timedelta(minutes=40) < sunset_today_time_utc
+    is_past_morning = utc_now > sunrise_time_utc + timedelta(minutes=30)
+    is_before_evening = utc_now + timedelta(minutes=40) < sunrise_time_utc
     is_sunny = sky_cover_value_now < 30
     is_windy = wind_speed_value_now in (WindSpeed.HIGH, WindSpeed.MEDIUM)
     
@@ -143,7 +124,7 @@ def main():
     if probability_of_precipitation_value_now > 10 or quantitative_precipitation_value_now > 0:
         print("Vivobarefoot shoes, Umbrella", sep="\n")
 
-    print(f"Next sunrise: {sunrise_statement}")
+    print(f"Sunrise: {sunrise_time_human_readable_statement}")
 
 
 main()
