e789fdadf781ddb834571cfe5494cf2f9b56b82f
Dan Friedman <dfriedman33@gmail.com>
2023-02-16 10:55:39 -0500

    parse sunset data and output it (#18)

--- external_apis/sunrise_sunset/schemas.py
+++ external_apis/sunrise_sunset/schemas.py
@@ -1,6 +1,7 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from typing import Union
 
+from dateutil import tz
 from pydantic import BaseModel, Field
 
 
@@ -28,6 +29,31 @@ class SunriseSunset(BaseModel):
     nautical_twilight_end: datetime
     astronomical_twilight_begin: datetime
     astronomical_twilight_end: datetime
+
+    @property
+    def sunrise_time_human_readable(self):
+        return self.get_humanized_time_string(self.sunrise_time_utc)
+
+    @property
+    def sunset_time_human_readable(self):
+        return self.get_humanized_time_string(self.sunset_time_utc)
+
+    @classmethod
+    def get_humanized_time_string(cls, datetime_value_utc: datetime):
+        # convert to local time
+        datetime_value_local = datetime_value_utc.astimezone(tz.tzlocal())
+        # converts to hour:minute am/pm
+        datetime_hour_minute = datetime_value_local.strftime("%-I:%M%p").lower()
+
+        datetime_now_utc = datetime.now(tz.tzutc())
+
+        if datetime_value_utc > datetime_now_utc:
+            output = f"is today at {datetime_hour_minute}"
+        elif datetime_value_utc > datetime_now_utc + timedelta(minutes=30):
+            output = f"was earlier at {datetime_hour_minute}"
+        else:
+            output = f"was way earlier at {datetime_hour_minute}"
+        return output
 
 
 class SunriseSunsetResponse(BaseModel):
