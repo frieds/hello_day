ecf98c52ec1467fbd0e1fddcd5838c2278079af1
Daniel Friedman <dfriedman33@gmail.com>
2023-02-21 18:53:06 -0500

    fix hour rounding

--- external_apis/weather/date_calculations.py
+++ external_apis/weather/date_calculations.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+
+
 def round_datetime_to_next_hour(datetime_obj):
     """
     Round a datetime object to the next hour.
@@ -5,7 +8,7 @@ def round_datetime_to_next_hour(datetime_obj):
     Otherwise, the hour is rounded down.
     """
     if datetime_obj.minute >= 40:
-        datetime_obj = datetime_obj.replace(hour=datetime_obj.hour + 1, minute=0, second=0, microsecond=0)
-    else:
-        datetime_obj = datetime_obj.replace(minute=0, second=0, microsecond=0)
+        # this handles edge condition of hour 23 utc + 1 hour = hour 0 (not hour 24)
+        datetime_obj += timedelta(hours=1)
+    datetime_obj = datetime_obj.replace(minute=0, second=0, microsecond=0)
     return datetime_obj
